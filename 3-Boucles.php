9218e0efe97fb1ea4544a2c11804e5b6c37ea661
dahrif <dahri.fatima.dev@gmail.com>
2021-03-29 09:12:33 +0100

    ajout de nouveaux fichiers

---
+++ 3-Boucles.php
@@ -0,0 +1,20 @@
+<?php
+// $nombre_de_lignes = 1;
+
+// while ($nombre_de_lignes <= 100)
+// {
+//     echo 'Je ne dois pas regarder les mouches voler quand j\'apprends le PHP.<br />';
+//     $nombre_de_lignes++; // $nombre_de_lignes = $nombre_de_lignes + 1
+// }
+
+
+
+
+
+
+
+for ($nombre_de_lignes = 1; $nombre_de_lignes <= 100; $nombre_de_lignes++)
+{
+    echo 'Ceci est la ligne n°' . $nombre_de_lignes . '<br />';
+}
+?>
